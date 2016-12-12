72ec1f4ff7d5cd8ca0ddb5f90badfd47d26c422b
boke1208 <907886076@qq.com>
2016-12-12 12:47:21 +0800

    服务器上的版本..

--- admindex.php
+++ admindex.php
@@ -1,10 +1,15 @@
+<?php
+    ini_set("display_errors", "On");
+    error_reporting(E_ALL | E_STRICT);
+    session_start();
+    date_default_timezone_set('Asia/Shanghai');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
   <title>心理咨询预约 - 北邮易班</title>
-
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="assets/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="assets/css/index.css" type="text/css" rel="stylesheet"/>
@@ -48,18 +53,11 @@
                     </tr>
                 </thead>
                 <tbody class="hoverable">
-<?php
-    $result = mysqli_query($con,"SELECT * FROM `psy` WHERE day='mon'");
-	mysqli_query($con,"set names utf8");
-	$num_result = mysqli_num_rows($result);
-	for ($i=0;$i<$num_result;$i++) {
-		$row = mysqli_fetch_row($result);
-        ?>
                     <tr class="hoverable">
-                        <td><?php echo $row[1]?></td>
-                        <td><?php echo $row[2]?></td>
-                        <td><?php echo $row[4]?></td>
-						<td><a class="waves-effect waves-teal btn-flat" href="information.php?id=<?php echo $row[0]?>">信息详情</a></td>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+						<td><a class="waves-effect waves-teal btn-flat" href="information.php?id=<?php echo 1;?>">信息详情</a></td>
 						<td><a class="waves-effect waves-teal btn-flat modal-trigger" href="#model1">成功预约</a>
 						  <div id="model1" class="modal">
                             <div class="modal-content">
@@ -72,195 +70,14 @@
                           </div>
 						</td>
                     </tr>
-			<?php
-        }
-?>
      	    </tbody>
             </table>
-	   </div>
-     </li>
-
-	 <li>
-       <div class="collapsible-header teal lighten-4">Tuesday</div>
-       <div class="collapsible-body">
-            <table class="hoverable">
-                <thead>
-                    <tr>
-                        <th data-field="id">姓名</th>
-                        <th data-field="name">联系方式</th>
-                        <th data-field="price">预约时间</th>
-                    </tr>
-                </thead>
-                <tbody class="hoverable">
-<?php
-    $result = mysqli_query($con,"SELECT * FROM `psy` WHERE day='tue'");
-	mysqli_query($con,"set names utf8");
-	$num_result = mysqli_num_rows($result);
-	for ($i=0;$i<$num_result;$i++) {
-		$row = mysqli_fetch_row($result);
-        ?>
-                    <tr class="hoverable">
-                        <td><?php echo $row[1]?></td>
-                        <td><?php echo $row[2]?></td>
-                        <td><?php echo $row[4]?></td>
-						<td><a class="waves-effect waves-teal btn-flat" href="information.php?id=<?php echo $row[0]?>">信息详情</a></td>
-						<td><a class="waves-effect waves-teal btn-flat modal-trigger" href="#model2">成功预约</a>
-						  <div id="model2" class="modal">
-                            <div class="modal-content">
-                               <h4>请确认！</h4>
-                               <p>确认成功预约后将删除此条记录哦</p>
-                            </div>
-                            <div class="modal-footer">
-                              <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">我知道啦</a>
-                            </div>
-                          </div>
-						</td>
-                    </tr>
-			<?php
-        }
-?>
-     	   </tbody>
-             </table>
-	   </div>
-     </li>
-
-	 <li>
-       <div class="collapsible-header teal lighten-3">Wednesday</div>
-       <div class="collapsible-body">
-            <table class="hoverable">
-                <thead>
-                    <tr>
-                        <th data-field="id">姓名</th>
-                        <th data-field="name">联系方式</th>
-                        <th data-field="price">预约时间</th>
-                    </tr>
-                </thead>
-                <tbody class="hoverable">
-<?php
-    $result = mysqli_query($con,"SELECT * FROM `psy` WHERE day='wens'");
-	mysqli_query($con,"set names utf8");
-	$num_result = mysqli_num_rows($result);
-	for ($i=0;$i<$num_result;$i++) {
-		$row = mysqli_fetch_row($result);
-        ?>
-                    <tr class="hoverable">
-                        <td><?php echo $row[1]?></td>
-                        <td><?php echo $row[2]?></td>
-                        <td><?php echo $row[4]?></td>
-						<td><a class="waves-effect waves-teal btn-flat" href="information.php?id=<?php echo $row[0]?>">信息详情</a></td>
-						<td><a class="waves-effect waves-teal btn-flat modal-trigger" href="#model3">成功预约</a>
-						  <div id="model3" class="modal">
-                            <div class="modal-content">
-                               <h4>请确认！</h4>
-                               <p>确认成功预约后将删除此条记录哦</p>
-                            </div>
-                            <div class="modal-footer">
-                              <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">我知道啦</a>
-                            </div>
-                          </div>
-						</td>
-                    </tr>
-			<?php
-        }
-?>
-     	   </tbody>
-             </table>
-	   </div>
-     </li>
-
-	 <li>
-       <div class="collapsible-header teal lighten-2">Thursday</div>
-       <div class="collapsible-body">
-            <table class="hoverable">
-                <thead>
-                    <tr>
-                        <th data-field="id">姓名</th>
-                        <th data-field="name">联系方式</th>
-                        <th data-field="price">预约时间</th>
-                    </tr>
-                </thead>
-                <tbody class="hoverable">
-<?php
-    $result = mysqli_query($con,"SELECT * FROM `psy` WHERE day='thurs'");
-	mysqli_query($con,"set names utf8");
-	$num_result = mysqli_num_rows($result);
-	for ($i=0;$i<$num_result;$i++) {
-		$row = mysqli_fetch_row($result);
-	//	$model=$row[0];
-        ?>
-                    <tr class="hoverable">
-                        <td><?php echo $row[1]?></td>
-                        <td><?php echo $row[2]?></td>
-                        <td><?php echo $row[4]?></td>
-						<td><a class="waves-effect waves-teal btn-flat" href="information.php?id=<?php echo $row[0]?>">信息详情</a></td>
-						<td><a class="waves-effect waves-teal btn-flat modal-trigger" href="#<?php echo $row[0]?>">成功预约</a>
-						  <div id="<?php $row[0]?>" class="modal">
-                            <div class="modal-content">
-                               <h4>请确认！</h4>
-                               <p>确认成功预约后将删除此条记录哦</p>
-                            </div>
-                            <div class="modal-footer">
-                              <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">我知道啦</a>
-                            </div>
-                          </div>
-						</td>
-                    </tr>
-			<?php
-        }
-?>
-     	   </tbody>
-             </table>
-	   </div>
-     </li>
-
-	 <li>
-       <div class="collapsible-header teal lighten-1">Friday</div>
-       <div class="collapsible-body">
-            <table class="hoverable">
-                <thead>
-                    <tr>
-                        <th data-field="id">姓名</th>
-                        <th data-field="name">联系方式</th>
-                        <th data-field="price">预约时间</th>
-                    </tr>
-                </thead>
-                <tbody class="hoverable">
-<?php
-    $result = mysqli_query($con,"SELECT * FROM `psy` WHERE day='fri'");
-	mysqli_query($con,"set names utf8");
-	$num_result = mysqli_num_rows($result);
-	for ($i=0;$i<$num_result;$i++) {
-		$row = mysqli_fetch_row($result);
-        ?>
-                    <tr class="hoverable">
-                        <td><?php echo $row[1]?></td>
-                        <td><?php echo $row[2]?></td>
-                        <td><?php echo $row[4]?></td>
-						<td><a class="waves-effect waves-teal btn-flat" href="information.php?id=<?php echo $row[0]?>">信息详情</a></td>
-						<td><a class="waves-effect waves-teal btn-flat modal-trigger" href="#model5">成功预约</a>
-						  <div id="model5" class="modal">
-                            <div class="modal-content">
-                               <h4>请确认！</h4>
-                               <p>确认成功预约后将删除此条记录哦</p>
-                            </div>
-                            <div class="modal-footer">
-                              <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">我知道啦</a>
-                            </div>
-                          </div>
-						</td>
-                    </tr>
-			<?php
-        }
-?>
-     	   </tbody>
-             </table>
 	   </div>
      </li>
     </ul>
 	</div>
 
     <div id="test2" class="col s12">
-
             <table class="hoverable">
                 <thead>
                     <tr>
@@ -272,24 +89,14 @@
                     </tr>
                 </thead>
                 <tbody class="hoverable">
-<?php
-    $result = mysqli_query($con,"SELECT * FROM `psy`");
-	mysqli_query($con,"set names utf8");
-	$num_result = mysqli_num_rows($result);
-	for ($i=0;$i<$num_result;$i++) {
-		$row = mysqli_fetch_row($result);
-        ?>
                     <tr class="hoverable">
-                        <td><?php echo $row[1]?></td>
-                        <td><?php echo $row[2]?></td>
-						<td><?php echo $row[3]?></td>
-                        <td><?php echo $row[4]?></td>
+                        <td><?php echo 1?></td>
+                        <td><?php echo 2?></td>
+						<td><?php echo 3?></td>
+                        <td><?php echo 4?></td>
 						<td>未完成（or）已完成</td>
-						<td><a class="waves-effect waves-teal btn-flat" href="information.php?id=<?php echo $row[0]?>">信息详情</a></td>
+						<td><a class="waves-effect waves-teal btn-flat" href="information.php?id=<?php echo 5?>">信息详情</a></td>
                     </tr>
-			<?php
-        }
-?>
      	   </tbody>
              </table>
 
@@ -313,6 +120,6 @@
   <script>  $(document).ready(function(){
     $('ul.tabs').tabs();
   });</script>
-  <script src="http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="https://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="assets/js/materialize.min.js"></script>
   <script type="text/javascript" src="assets/js/getexcel.js"></script>
